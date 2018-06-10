53ff182e59f7cd1cd597ad211aa5d7588874d41b
vipomage <vipomage@gmail.com>
2018-06-10 17:30:25 +0300

    09.Exercise

---
+++ 09.ExerciseExpress.Js/Exercise/source/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fileUploader = require('express-fileupload');
+
+const homeModule = require('./modules/homeModule');
+const memeModule = require('./modules/memeModule');
+const apiModule = require('./modules/apiModule');
+
+const port = 2323;
+
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUploader());
+
+app.use('/', homeModule);
+app.use('/memes', memeModule);
+app.use('/api', apiModule);
+
+require('./config/mongoConfig')
+  .then(() => {
+    app.listen(port, () =>
+      console.log('Im listening on ' + port + `\nhttp://localhost:${port}`)
+    );
+  })
+  .catch(err => {
+    console.log('Could not connect to MongoDb\n', err);
+  });
